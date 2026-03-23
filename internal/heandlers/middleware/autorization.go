2584fe81e81b78f2ab90e60442206ab9086c5538
Lucas Henrique <lucashenriquecalixto355@gmail.com>
2026-03-23 20:27:13 -0300

    Add core and supporting dependencies to project
    Introduces essential libraries for routing, logging, and API handling,
    as well as indirect dependencies for schema parsing and system interface
    support, preparing the project for expanded web API functionality.

---
+++ internal/heandlers/middleware/autorization.go
@@ -0,0 +1,22 @@
+package middleware
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/avukadin/goapi/internal/tools"
+	log "github.com/sirupsen/logrus"
+)
+
+var UnAuthorizedError = errors.new("Invalid username or token")
+func Authorization (next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter,r *http.Request ) {
+		var username string = r.URL.Query().Get("username")
+		var token = r.Header.Get("Authorization")
+		var err error
+
+		if username == "" || token == "" {
+			
+		}
+	})
+}
